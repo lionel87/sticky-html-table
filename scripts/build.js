a27b17249952335cd9cab7544e1488af3b4f143b
Bulik László <laszlo.bulik@ksh.hu>
2023-04-17 16:56:16 +0200

    create package and build

---
+++ scripts/build.js
@@ -0,0 +1,37 @@
+import esbuild from 'esbuild';
+import { sassPlugin } from 'esbuild-sass-plugin';
+import path from 'path';
+
+const args = process.argv.slice(2);
+
+const watch = args.includes('--watch');
+const serve = args.includes('--serve');
+const production = args.includes('--env=production') || process.env.NODE_ENV === 'production';
+
+esbuild[(watch || serve) ? 'context' : 'build']({
+	logLevel: 'info',
+	entryPoints: ['src/sticky-table.ts', 'src/sticky-table.scss'],
+	bundle: true,
+	format: 'iife',
+	splitting: false,
+	treeShaking: true,
+	minify: production,
+	outdir: 'dist',
+	sourcemap: false,
+	legalComments: 'eof',
+	target: ['chrome58', 'firefox57', 'safari11', 'edge18'],
+	plugins: [
+		sassPlugin({
+			precompile(source, pathname) {
+				const basedir = path.dirname(pathname).replaceAll('\\', '/');
+				return source.replace(/(url\(['"]?)(\.\.?\/)([^'")]+['"]?\))/g, `$1${basedir}/$2$3`);
+			}
+		}),
+	],
+}).then((ctx) => {
+	watch && ctx.watch();
+	serve && ctx.serve({
+		port: 80,
+		servedir: 'dist',
+	});
+});
